ab9398eead9d7a403da5de0daca32dc9fb4ad881
HamimHossain <hamim.mgp@gmail.com>
2023-04-20 08:41:23 +0600

    Initial set up done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 // import './App.css';
 
+import AirlinesCard from "./components/AirlinesCard/AirlinesCard";
+import Navbar from "./components/Navbar/Navbar"
+
 function App() {
   return (
     <div className="App">
       <h1>Hello world !</h1>
+      <Navbar></Navbar>
+      <AirlinesCard></AirlinesCard>
     </div>
   );
 }
---
+++ src/components/AirlinesCard/AirlinesCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AirlinesCard = () => {
+    return (
+        <div>
+            <h1>This is from Airlines Card....!!!</h1>
+        </div>
+    );
+};
+
+export default AirlinesCard;
